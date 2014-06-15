90685756c32d31f57ca73486c2ae37c243f1c035
Xiao Li <swing1979@gmail.com>
2014-06-14 19:52:43 -0700

    fix http post in ruby 1.8.7, add more error log for post failures

--- lib/corn/post.rb
+++ lib/corn/post.rb
@@ -19,7 +19,7 @@ module Corn
             sleep 1
           end
         rescue => e
-          log("Corn post thread stopped by error #{e.message}\n#{e.backtrace.join("\n")}")
+          Corn.logger.error("Corn post thread stopped by error #{e.message}\n#{e.backtrace.join("\n")}")
         end
       end
     end
@@ -29,25 +29,26 @@ module Corn
     end
 
     def http_post(data, name)
-      url = URI.parse(submit_url)
-      req = Net::HTTP::Post.new(url.path)
+      uri = URI.parse(submit_url)
+      req = Net::HTTP::Post.new(uri.path)
       req.set_form_data("data" => data, 'client_id' => Corn.client_id, 'name' => name)
-      res = Net::HTTP.start(url.host, url.port) do |http|
-        http.use_ssl = url.scheme == 'https'
-        http.request(req)
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      if uri.scheme == 'https'
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE if ENV['CORN_SKIP_SSL_VERIFY']
       end
-      log("Corn report submitted to #{submit_url}")
+      res = http.request(req)
+      Corn.logger.info("Corn report submitted to #{submit_url}")
+      unless res.is_a?(Net::HTTPSuccess)
+        Corn.logger.error("Post failed: #{res.message}(#{res.code}), response body: \n#{res.body}")
+      end
     rescue Exception => e
-      log("post to #{submit_url} failed: #{e.message}")
-      log(e.backtrace.join("\n"))
+      Corn.logger.error("post to #{submit_url} failed: #{e.message}\n#{e.backtrace.join("\n")}")
     end
 
     def submit_url
       Corn.submit_url
-    end
-
-    def log(msg)
-      Corn.logger.info msg
     end
   end
 end
